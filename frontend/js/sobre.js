6856b83178023ed0b1651228948264be5100300d
Ana <poopys.al.br@gmail.com>
2026-05-15 21:02:02 -0300

    First commit: estrutura completa do frontend.

---
+++ frontend/js/sobre.js
@@ -0,0 +1,7 @@
+function handleDoar() {
+    alert('Obrigado pelo interesse! Nossa área de doações financeiras e de ração estará disponível assim que integrarmos o sistema.');
+}
+
+function handleVoluntario() {
+    alert('Ficamos muito felizes! Em breve abriremos o formulário de inscrição para novos voluntários no campus.');
+}
