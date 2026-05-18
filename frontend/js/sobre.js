d8a4b84500e31578385b2ab3cf20e0225961abc0
Ana <poopys.al.br@gmail.com>
2026-05-18 19:52:53 -0300

    Removendo a pasta antiga frontend

--- frontend/js/sobre.js
+++
@@ -1,7 +0,0 @@
-function handleDoar() {
-    alert('Obrigado pelo interesse! Nossa área de doações financeiras e de ração estará disponível assim que integrarmos o sistema.');
-}
-
-function handleVoluntario() {
-    alert('Ficamos muito felizes! Em breve abriremos o formulário de inscrição para novos voluntários no campus.');
-}
